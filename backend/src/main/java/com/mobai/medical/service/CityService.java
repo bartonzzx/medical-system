d47fb4889c1ba28ade5bf77e0e78f21d0f66b18a
mintoneko <mobaisilent@gmail.com>
2025-07-05 12:06:02 +0800

    complete task 10

--- backend/src/main/java/com/mobai/medical/service/CityService.java
+++ backend/src/main/java/com/mobai/medical/service/CityService.java
@@ -3,12 +3,15 @@ package com.mobai.medical.service;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.mobai.medical.domain.City;
+import com.mobai.medical.entity.CityEntity;
 import com.mobai.medical.mapper.CityMapper;
 import com.mobai.medical.model.CityModel;
 import com.mobai.medical.utils.Msg;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -37,5 +40,37 @@ public class CityService {
       return Msg.fail().mess("没有找到");
     }
     return Msg.success().data("city", city);
+  }
+
+  // 添加一个城市
+  public Msg saveCity(Integer cityNumber) {
+    City city = new City();
+    Date d = new Date();
+    city.setCityNumber(cityNumber);
+    city.setCreatetime(d);
+    city.setUpdatetime(d);
+    CityEntity ce = new CityEntity();
+    BeanUtils.copyProperties(city, ce); //对象拷贝
+    int i = cityMapper.saveCity(ce);
+    if (i > 0) {
+      Long num = ce.getTotal() % 5 == 0 ? (ce.getTotal() / 5) : (ce.getTotal() / 5) + 1;
+      return Msg.success().data("pages", num).mess("添加成功");
+    }
+    return Msg.fail().mess("添加成功");
+  }
+
+  // 传入id，删除一个城市
+  public Msg deleteCityById(Integer id) {
+    int i = cityMapper.deleteCityById(id);
+    if (i > 0) {
+      return Msg.success().mess("删除成功");
+    } else {
+      return Msg.fail().mess("删除失败");
+    }
+  }
+
+  // 传入id，检查城市名是否存在
+  public int checkCityByName(Integer number) {
+    return cityMapper.checkCityByName(number);
   }
 }
