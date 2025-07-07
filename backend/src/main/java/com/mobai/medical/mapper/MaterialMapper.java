32d7482a7c9d0796ecd6d38296b0b0b8c927e5a7
mintoneko <mobaisilent@gmail.com>
2025-07-07 20:35:32 +0800

    complete task 18

--- backend/src/main/java/com/mobai/medical/mapper/MaterialMapper.java
+++ backend/src/main/java/com/mobai/medical/mapper/MaterialMapper.java
@@ -1,5 +1,6 @@
 package com.mobai.medical.mapper;
 
+import com.mobai.medical.entity.MaterialEntity;
 import com.mobai.medical.model.MaterialModel;
 import com.mobai.medical.param.MaterialParam;
 import org.springframework.stereotype.Component;
@@ -12,4 +13,13 @@ public interface MaterialMapper {
 
   // 查询所有的材料
   List<MaterialModel> getAllMaterial(MaterialParam param);
+
+  // 新增材料
+  int saveMaterial(MaterialEntity entity);
+
+  // 更新材料
+  int updateMaterial(MaterialEntity entity);
+
+  // 删除材料
+  int deleteMaterial(Long id);
 }
--- backend/src/main/java/com/mobai/medical/service/MaterialService.java
+++ backend/src/main/java/com/mobai/medical/service/MaterialService.java
@@ -2,10 +2,13 @@ package com.mobai.medical.service;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.mobai.medical.entity.MaterialEntity;
 import com.mobai.medical.mapper.MaterialMapper;
 import com.mobai.medical.model.MaterialModel;
 import com.mobai.medical.param.MaterialParam;
 import com.mobai.medical.utils.Msg;
+import org.joda.time.DateTime;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,5 +29,43 @@ public class MaterialService {
     List<MaterialModel> list = materialMapper.getAllMaterial(param);
     PageInfo<MaterialModel> info = new PageInfo<>(list, 5);
     return Msg.success().data("materialInfo", info);
+  }
+
+  // 添加必备材料
+  public Msg saveMaterial(MaterialParam param) {
+    param.setCreateTime(new DateTime().toDate());
+    param.setUpdateTime(new DateTime().toDate());
+    MaterialEntity mEntity = new MaterialEntity();
+    BeanUtils.copyProperties(param, mEntity);
+    int i = materialMapper.saveMaterial(mEntity);
+    List<MaterialModel> allMaterial = materialMapper.getAllMaterial(null);
+    mEntity.setTotal((long) allMaterial.size());
+    if (i > 0) {
+      Long num = mEntity.getTotal() % 5 == 0 ? (mEntity.getTotal() / 5) : (mEntity.getTotal() / 5) + 1;
+      return Msg.success().mess("添加成功").data("numberOfAdd", i).data("pages", num);
+    }
+    return Msg.fail().mess("添加失败");
+  }
+
+  // 更新必备材料
+  public Msg updateMaterial(Integer id, MaterialParam param) {
+    MaterialEntity entity = new MaterialEntity();
+    BeanUtils.copyProperties(param, entity);
+    entity.setUpdateTime(new DateTime().toDate());
+    entity.setId(id);
+    int i = materialMapper.updateMaterial(entity);
+    if (i > 0) {
+      return Msg.success().mess("修改成功").data("updateData", entity);
+    }
+    return Msg.fail().mess("修改失败");
+  }
+
+  // 根据id删除必备材料
+  public Msg deleteMaterial(Long id) {
+    int i = materialMapper.deleteMaterial(id);
+    if (i > 0) {
+      return Msg.success().mess("删除成功").data("numberOfDelete", i);
+    }
+    return Msg.fail().mess("删除失败");
   }
 }
