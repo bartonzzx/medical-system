d18e7aa2c15e07389713993b603417d675df9251
bartonzzx <barton_zhang@outlook.com>
2025-07-09 21:54:58 +0800

    新增城市管理mcp接口

---
+++ agent/backend/medical-mcp/CityManagement.py
@@ -0,0 +1,5 @@
+from typing import Any
+import httpx
+import config
+from main import mcp
+
--- agent/backend/medical-mcp/main.py
+++ agent/backend/medical-mcp/main.py
@@ -5,6 +5,7 @@ mcp = FastMCP("medical-mcp", host='0.0.0.0', port=8081)
 from DoctorManagement import *
 from CompanyManagement import *
 from CompanyPolicyManagement import *
+from CityManagement import *
 
 if __name__ == "__main__":
     # 初始化并运行 server
